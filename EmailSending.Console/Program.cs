d90f1d92ca4d8436ebfd7d8a2fddfb02630fa445
Franck Quintana <franck.quintana@gmail.com>
2021-06-07 20:51:46 +0200

    Add code

--- EmailSending.Console/Program.cs
+++ EmailSending.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace EmailSending.Console
 {
@@ -6,7 +8,30 @@ namespace EmailSending.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var message = new MailMessage
+            {
+                Subject = "Test subject",
+                Body = "Test body",
+                From = new MailAddress("elmenjardaqui@gmail.com", "El Menjar d'Aqui")
+            };
+            message.To.Add("franck.quintana@gmail.com");
+            try
+            {
+                using (var sender = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    sender.UseDefaultCredentials = false;
+                    sender.Credentials = new NetworkCredential("elmenjardaqui@gmail.com", "password");
+                    sender.EnableSsl = true;
+                    sender.Send(message);
+                }
+
+                System.Console.WriteLine("Message sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Error: " + ex.Message);
+                throw;
+            }
         }
     }
 }
